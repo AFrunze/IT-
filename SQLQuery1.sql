37ec90ce19a59f51102e5af5c2adb11290b1e869
AFrunze <45385572+AFrunze@users.noreply.github.com>
2019-02-01 19:55:44 +0300

    part 1 SQL

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -1,3 +1,5 @@
+--Установим драйвер Microsoft.ACE.OLEDB.12.0
+--необходимо включить параметр ad hoc distributed queries в конфигурации сервера
 sp_configure 'show advanced options', 1;
 RECONFIGURE;
 GO
@@ -5,6 +7,7 @@ sp_configure 'Ad Hoc Distributed Queries', 1;
 RECONFIGURE;
 GO
 
+--В приведенном ниже примере кода данные импортируются из листа Excel base в новую таблицу базы данных с помощью OPENROWSET
 USE ImportFromExcel;
 GO
 SELECT * INTO base_dq
